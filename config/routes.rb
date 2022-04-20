0d45473f25eb50bc73646300db1895c2c35dd4b1
JJ Kong <20kongjj@gmail.com>
2022-04-20 15:48:16 -0400

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -14,16 +14,12 @@ Rails.application.routes.draw do
   get 'admin_pages/teams'
   
 
-<<<<<<< HEAD
   # comment out this root to merge back with normal functionality'
   root 'home_pages#home'
 
 
   # uncomment out to retun to normal functionality
   #root 'home_pages#home'
-=======
-  root 'home_pages#home'
->>>>>>> refs/remotes/origin/main
 
 
   resources :student_on_teams
