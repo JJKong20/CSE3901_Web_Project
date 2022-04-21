75fac79d9058a585c5a0bfc5314df2093ce4fd04
Nicholas Llewellyn <nllewellyn@remanencetg.com>
2022-04-21 16:06:37 -0400

    committing bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_21_183117) do
+ActiveRecord::Schema.define(version: 2022_04_21_174230) do
 
   create_table "admins", force: :cascade do |t|
     t.string "fname"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2022_04_21_183117) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "admin_id", null: false
     t.string "password_digest"
-    t.string "password"
     t.index ["admin_id"], name: "index_students_on_admin_id"
   end
 
