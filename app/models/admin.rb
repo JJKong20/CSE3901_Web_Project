f1b1c177fc1fb84cd63cd2ac1a3b620595bfea7f
YUFULEO <97569688+YUFULEO@users.noreply.github.com>
2022-04-23 17:49:50 -0400

    Update admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,11 +1,12 @@
 class Admin < ApplicationRecord
 
     before_save { self.email = email.downcase }
-    validates :fname, presence: true
-    validates :lname, presence: true
+    validates :fname, presence: true, length: { maximum: 50 }
+    validates :lname, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, 
-    format: { with: VALID_EMAIL_REGEX }, 
+    format: { with: VALID_EMAIL_REGEX },
+    length: { maximum: 255 },
     uniqueness: true
     has_secure_password
     validates :password, presence: true, length: {minimum: 5}
