c8f9746c112c596d908dd6bbc247a0c425cd2803
YUFULEO <97569688+YUFULEO@users.noreply.github.com>
2022-04-23 17:50:35 -0400

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,6 +13,7 @@ class Student < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
   format: { with: VALID_EMAIL_REGEX }, 
+  length: { maximum: 255 },
   uniqueness: true
   has_secure_password(validations: false)
   validates :password, presence: true, length: { minimum: 5 }, on: :signup
